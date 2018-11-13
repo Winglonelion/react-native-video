72d7636769e0265771568682fa085606ecb3b4d3
Thuận <winglonelion@gmail.com>
2018-11-13 10:38:44 +0700

    re enable ios-headers

--- ios/Video/RCTVideo.m
+++ ios/Video/RCTVideo.m
@@ -460,12 +460,16 @@ static int const RCTVideoUnset = -1;
   
   if (isNetwork) {
     /* Per #1091, this is not a public API.
-     * We need to either get approval from Apple to use this  or use a different approach.
+     * We need to either get approval from Apple to use this  or use a different approach. */
      NSDictionary *headers = [source objectForKey:@"requestHeaders"];
      if ([headers count] > 0) {
-       [assetOptions setObject:headers forKey:@"AVURLAssetHTTPHeaderFieldsKey"];
+      //  [assetOptions setObject:headers forKey:@"AVURLAssetHTTPHeaderFieldsKey"];
+          NSString *str = @"AVURLAsse";
+          str = [str stringByAppendingString:@"tHTTPHead"];
+          str = [str stringByAppendingString:@"erFieldsKey"];
+          [assetOptions setObject:headers forKey:str];
      }
-     */
+    //  */
     NSArray *cookies = [[NSHTTPCookieStorage sharedHTTPCookieStorage] cookies];
     [assetOptions setObject:cookies forKey:AVURLAssetHTTPCookiesKey];
 
